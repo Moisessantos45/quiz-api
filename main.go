5fe34fddbc12975aecc6e81e199950b1c1e05f9b
Moisessantos45 <santosxphdz34@gmail.com>
2026-05-05 01:36:46 -0600

    fix: update main.go

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 		AllowWebSockets:  true,
 	}))
 
-	//r.Use(middleware.RateLimiterMiddleware(rate.Every(time.Second/10), 50))
+	 //r.Use(middleware.RateLimiterMiddleware(rate.Every(time.Second/10), 50))
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Welcome to the Quiz API"})
 	})
